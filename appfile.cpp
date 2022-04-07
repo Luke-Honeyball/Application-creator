fefe05ab091015b3c660ce6a0fc714bbe4faa425
LukeTheCodeMaker <luke.honeyball.03@gmail.com>
2022-04-07 10:48:59 +0200

    added app

---
+++ appfile.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "enter ur name: ";
+    cin >> name;
+    cout << "\n\nYour name is: " << name;
+    return 0;
+}
